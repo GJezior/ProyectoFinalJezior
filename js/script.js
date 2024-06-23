7470a6e6d1626ece434da45fc3b7d6bdbc81045e
Gustavo Jezior <gustavo.jezior@gmail.com>
2024-06-23 12:17:06 -0300

    ajustes del js con fetch local

--- js/script.js
+++ js/script.js
@@ -1,136 +1,139 @@
-//Defino un listado de productos como objetos ya con sus img definidas para utlizarse desde el js por medio del DOM
+//Defino un listado de productos de forma local y los cargo por medio del uso de fetch 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const productos = {
-        "1": { nombre: "Fender Vintage II 1961 Stratocaster - Fiesta Red", precio: 2299, img: "./img/fenderstratocaster60.jpg" },
-        "2": { nombre: "Fender Pro II Telecaster - Butterscotch Blonde", precio: 1899, img: "./img/fendertelecasterpro.jpg" },
-        "3": { nombre: "Gibson Les Paul Standard '60s - Honey Amber", precio: 2799, img: "./img/gibsonlespaulstandard60.jpg" },
-        "4": { nombre: "Gibson ES-335 Semi-hollowbody - Sixties Cherry", precio: 3499, img: "./img/gibsones335.jpg" },
-        "5": { nombre: "Ibanez Genesis RG550 - Desert Sun Yellow", precio: 999, img: "./img/ibanez550.jpg" },
-        "6": { nombre: "Ibanez Prestige AZ2204NW - Pastel Pink Rose", precio: 1999, img: "./img/ibanezaz2204.jpg" }
-    };
-
-    const contenedorProductos = document.getElementById("productos-container");
+    fetch('./productos.json')
+        .then(response => response.json())
+        .then(productos => {
+            const contenedorProductos = document.getElementById("productos-container");
 
 //Aplico como sera el diseño y la visualizacion de nuestros productos
 
-    const renderizarProductos = () => {
-        contenedorProductos.innerHTML = "";
+            const renderizarProductos = () => {
+                contenedorProductos.innerHTML = "";
 
-        for (const id in productos) {
-            const producto = productos[id];
-            const tarjetaProducto = `
-                <div class="col mb-4">
-                    <div class="card custom-card">
-                        <img src="${producto.img}" alt="${producto.nombre}" class="card-img-top mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title mb-0">${producto.nombre}</h5>
+                productos.forEach(producto => {
+                    const tarjetaProducto = `
+                        <div class="col mb-4">
+                            <div class="card custom-card">
+                                <img src="${producto.imagen}" alt="${producto.nombre}" class="card-img-top mb-3">
+                                <div class="card-body">
+                                    <h5 class="card-title mb-0">${producto.nombre}</h5>
+                                </div>
+                                <div class="card-body d-flex justify-content-around">
+                                    <p class="fs-3 fw-bold mb-0">$${producto.precio}</p>
+                                    <button class="btn btn-outline-light" data-id="${producto.id_producto}">Agregar al carrito</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body d-flex justify-content-around">
-                            <p class="fs-3 fw-bold mb-0">$${producto.precio}</p>
-                            <button class="btn btn-outline-light" data-id="${id}">Agregar al carrito</button>
-                        </div>
-                    </div>
-                </div>
-            `;
-            contenedorProductos.insertAdjacentHTML("beforeend", tarjetaProducto);
-        }
+                    `;
+                    contenedorProductos.insertAdjacentHTML("beforeend", tarjetaProducto);
+                });
 
-        document.querySelectorAll(".btn-outline-light").forEach(boton => {
-            boton.addEventListener("click", agregarAlCarrito);
-        });
-    };
+                document.querySelectorAll(".btn-outline-light").forEach(boton => {
+                    boton.addEventListener("click", agregarAlCarrito);
+                });
+            };
 
 //Defino el evento del carrito de compras utilizando JSON y estructuro los diferentes casos posibles de seleccion
 
-    const agregarAlCarrito = (event) => {
-        const productoId = event.target.getAttribute("data-id");
-        let carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
-        const productoExistente = carrito.find(item => item.id === productoId);
+            const agregarAlCarrito = (event) => {
+                const productoId = event.target.getAttribute("data-id");
+                let carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
+                const productoExistente = carrito.find(item => item.id == productoId);
 
-        if (productoExistente) {
-            productoExistente.cantidad += 1;
-        } else {
-            carrito = [...carrito, { id: productoId, cantidad: 1 }];
-        }
+                if (productoExistente) {
+                    productoExistente.cantidad += 1;
+                } else {
+                    carrito = [...carrito, { id: productoId, cantidad: 1 }];
+                }
 
-        localStorage.setItem("productosSeleccionados", JSON.stringify(carrito));
-        actualizarVisualizacionCarrito();
+                localStorage.setItem("productosSeleccionados", JSON.stringify(carrito));
 
-//Utilizo toastify para generar un mensaje emergente al presionar el boton "agregar al carrito" y defino su estilo
+//Utilizo de la libreria toastify para generar un mensaje emergente al presionar el boton "agregar al carrito" y defino su estilo
 
-        Toastify({
-            text: "Producto agregado al carrito",
-            className: "info",
-            style: {
-                background: "#444",
-            }
-        }).showToast();
-    };
+                actualizarVisualizacionCarrito().then(() => {
+                    Toastify({
+                        text: "Producto agregado al carrito",
+                        className: "info",
+                        style: {
+                            background: "#444",
+                        },
+                        duration: 3000
+                    }).showToast();
+                });
+            };
 
-    const eliminarDelCarrito = (productoId) => {
-        let carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
-        carrito = carrito.filter(item => item.id !== productoId);
-        localStorage.setItem("productosSeleccionados", JSON.stringify(carrito));
-        actualizarVisualizacionCarrito();
-    };
+// Agrego la posibilidad de eliminar productos del carrito de compras
+
+            const eliminarDelCarrito = (productoId) => {
+                let carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
+                carrito = carrito.filter(item => item.id != productoId);
+                localStorage.setItem("productosSeleccionados", JSON.stringify(carrito));
+                actualizarVisualizacionCarrito();
+            };
 
 // Seteo la calculadora del carrito de compras para sumar el total de valor de los productos seleccionados
 
-    const actualizarVisualizacionCarrito = () => {
-        const carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
-        const contenedorProductosCompra = document.getElementById("productosCompra");
-        const totalElemento = document.getElementById("total");
+            const actualizarVisualizacionCarrito = () => {
+                return new Promise((resolve) => {
+                    const carrito = JSON.parse(localStorage.getItem("productosSeleccionados")) || [];
+                    const contenedorProductosCompra = document.getElementById("productosCompra");
+                    const totalElemento = document.getElementById("total");
 
-        contenedorProductosCompra.innerHTML = "";
-        let total = 0;
+                    contenedorProductosCompra.innerHTML = "";
+                    let total = 0;
 
-        carrito.forEach(item => {
-            const producto = productos[item.id];
-            total += producto.precio * item.cantidad;
+                    carrito.forEach(item => {
+                        const producto = productos.find(p => p.id_producto == item.id);
+                        total += producto.precio * item.cantidad;
 
-            const itemCarrito = `
-                <div>
-                    <div class="img">
-                        <img src="${producto.img}" alt="${producto.nombre}">
-                        <span>${producto.nombre}</span>
-                    </div>
-                    <div>
-                        <span>${item.cantidad} x $${producto.precio}</span>
-                        <button class="btn-remover" data-id="${item.id}">x</button>
-                    </div>
-                </div>
-            `;
-            contenedorProductosCompra.insertAdjacentHTML("beforeend", itemCarrito);
-        });
+                        const itemCarrito = `
+                            <div>
+                                <div class="img">
+                                    <img src="${producto.imagen}" alt="${producto.nombre}">
+                                    <span>${producto.nombre}</span>
+                                </div>
+                                <div>
+                                    <span>${item.cantidad} x $${producto.precio}</span>
+                                    <button class="btn-remover" data-id="${item.id}">x</button>
+                                </div>
+                            </div>
+                        `;
+                        contenedorProductosCompra.insertAdjacentHTML("beforeend", itemCarrito);
+                    });
 
-        totalElemento.innerHTML = `Total: $${total}`;
+                    totalElemento.innerHTML = `Total: $${total}`;
 
-        document.querySelectorAll(".btn-remover").forEach(boton => {
-            boton.addEventListener("click", (e) => {
-                const productoId = e.target.getAttribute("data-id");
-                eliminarDelCarrito(productoId);
-            });
-        });
-    };
+                    document.querySelectorAll(".btn-remover").forEach(boton => {
+                        boton.addEventListener("click", (e) => {
+                            const productoId = e.target.getAttribute("data-id");
+                            eliminarDelCarrito(productoId);
+                        });
+                    });
+
+                    resolve();
+                });
+            };
 
 // Arranque de las funciones para la visualizacion de los productos al cargar la pagina 
 
 renderizarProductos();
 actualizarVisualizacionCarrito();
 
-// Defino acciones al seleccionar distintos elementos del carrito de compras
+// Defino lo que ocurrira al seleccionar distintos elementos que estan dentro del carrito de compras
 
-    const iconoCarrito = document.getElementById("carritoIcono");
-    const contenedorCompra = document.getElementById("contenedorCompra");
-    const botonCerrar = document.getElementById("x");
+            const iconoCarrito = document.getElementById("carritoIcono");
+            const contenedorCompra = document.getElementById("contenedorCompra");
+            const botonCerrar = document.getElementById("x");
 
-    iconoCarrito.addEventListener("click", (e) => {
-        e.preventDefault();
-        contenedorCompra.style.display = "flex";
-    });
+            iconoCarrito.addEventListener("click", (e) => {
+                e.preventDefault();
+                contenedorCompra.style.display = "flex";
+            });
 
-    botonCerrar.addEventListener("click", () => {
-        contenedorCompra.style.display = "none";
-    });
+            botonCerrar.addEventListener("click", () => {
+                contenedorCompra.style.display = "none";
+            });
+        })
+        .catch(error => console.error('Error al cargar el JSON de productos:', error));
 });
